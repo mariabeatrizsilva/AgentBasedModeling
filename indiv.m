14406f5a2d7ab3073afc750810ee6dd618b9b9e4
Maria Beatriz <ms14127@nyu.edu>
2024-03-25 12:42:53 -0400

    Create indiv.m

---
+++ indiv.m
@@ -0,0 +1,21 @@
+classdef indiv 
+    %defines an individual with a position (x,y) and a grp (SIR)
+    properties 
+        pos = zeros(2);
+        grp = 'S';
+    end 
+    methods 
+        function obj = set.pos(obj, vpos)
+            obj.pos = vpos; 
+        end 
+        function obj = set.grp(obj, ingrp)
+            obj.grp = ingrp; 
+        end 
+        function value = getPos(obj)
+            disp("getting pos")
+            value = obj.pos;
+        end
+        %function move(obj, step, xBound, yBound)
+    end 
+end
+
